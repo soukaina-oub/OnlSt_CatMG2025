73f2afc962c46ac1b3b9ad14c507e28dc6735ae4
soukaina <soukaina@DESKTOP-AB67MJN>
2025-01-24 20:55:11 -0500

    j'ai modifier celui la

--- OnlSt_CatMG2025/app.py
+++ OnlSt_CatMG2025/app.py
@@ -12,14 +12,15 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+	"""Renders a sample page."""
+	return "Hello World!"
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+	import os
+	HOST = os.environ.get('SERVER_HOST', 'localhost')
+	try:
+		PORT = int(os.environ.get('SERVER_PORT', '5555'))
+	except ValueError:
+		PORT = 5555
+	app.run(HOST, PORT)
+	
